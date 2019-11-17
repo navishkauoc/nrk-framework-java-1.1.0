76435edf698199a305983c719bef97e1da9e3ef7
Navishka Kularathna <navishka.randika@gmail.com>
2019-11-17 16:28:09 +0530

    CommonController.java

--- FrameworkNRK/src/com/navishkakularathna/frameworknrk/CommonController.java
+++ FrameworkNRK/src/com/navishkakularathna/frameworknrk/CommonController.java
@@ -1,7 +1,32 @@
 package com.navishkakularathna.frameworknrk;
 
+import java.lang.reflect.Field;
+
 public class CommonController {
-	public static void insert(Object obj) {
+	public static String insert(Object obj) {
+		Class c = obj.getClass();
+		String name = c.getSimpleName().toLowerCase();
 		
+		String names = "";
+		String values = "";
+		
+		Field[] fields = c.getFields();
+		for(Field field : fields) {
+			String fName = field.getName();
+			names += fName + ",";
+			try {
+				Object value = field.get(obj);
+				values += "'" + value.toString() + "'";
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		names = names.substring(0, names.length() - 1);
+		values = values.substring(0, values.length() - 1);
+		
+		String sql = "INSERT INTO " + name + " (" + names + ") VALUES (" + values + ")";
+		
+		return sql;
 	}
 }
