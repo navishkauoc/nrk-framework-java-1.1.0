43037e2e881f0eecf5f125e4a3062135362c623a
Navishka Kularathna <navishka.randika@gmail.com>
2019-11-17 13:28:26 +0530

    Student.java

---
+++ FrameworkNRK/src/com/navishkakularathna/frameworknrk/CommonController.java
@@ -0,0 +1,5 @@
+package com.navishkakularathna.frameworknrk;
+
+public class CommonController {
+
+}
---
+++ FrameworkNRK/src/com/navishkakularathna/frameworknrk/demo/Student.java
@@ -0,0 +1,9 @@
+package com.navishkakularathna.frameworknrk.demo;
+
+public class Student {
+	public int id;
+	public String name;
+	public String nic;
+	public String mobile;
+	public String email;
+}
