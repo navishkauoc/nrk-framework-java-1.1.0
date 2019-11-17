06d78adbd9604e10774c5dac6ec9cec78bb92935
Navishka Kularathna <navishka.randika@gmail.com>
2019-11-17 13:35:26 +0530

    student object passed to CommonController

--- FrameworkNRK/src/com/navishkakularathna/frameworknrk/demo/Demo.java
+++ FrameworkNRK/src/com/navishkakularathna/frameworknrk/demo/Demo.java
@@ -1,5 +1,16 @@
 package com.navishkakularathna.frameworknrk.demo;
 
-public class Demo {
+import com.navishkakularathna.frameworknrk.CommonController;
 
+public class Demo {
+	public static void main(String args[]) {
+		Student student = new Student();
+		student.id = 1;
+		student.name = "Nicky Barnes";
+		student.nic = "234567235g";
+		student.email = "nickyb@gmail.com";
+		student.mobile = "0772344563";
+		
+		CommonController.insert(student);
+	}
 }
